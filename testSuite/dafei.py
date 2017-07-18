19bf99901bfb93071c19e0b40fa8ac2bdf9cdca4
xuchu <xin7c@qq.com>
2017-07-18 09:52:30 +0800

    upup 20170718

--- testSuite/dafei.py
+++ testSuite/dafei.py
@@ -4,18 +4,19 @@
 """
 @author: xuchu
 @software: PyCharm
-@file: dafei001.py
+@file: dafei.py
 @time: 2017/6/20 下午5:32
 """
 import os
 import sys
+
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
 from utils.jmeterRunner import Action
 import config_global as cg
 
 dafei001 = Action()
 dafei001.runJmeter(jmx_path=dafei001.jmx("dafei001.jmx"),
-                var=dafei001.j_var(cg.j_Vars))
+                   var=dafei001.j_var(cg.j_Vars))
 
 dafei001.runJmeter(jmx_path=dafei001.jmx("dafei002.jmx"),
-                var=dafei001.j_var(cg.j_Vars))
+                   var=dafei001.j_var(cg.j_Vars))
